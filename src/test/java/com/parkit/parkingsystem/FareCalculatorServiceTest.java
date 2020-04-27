927fc7af79ddce506afd6c1cb6dce174a874dfe5
olivier.bonheur <olivier.bonheur@gmail.com>
2020-04-27 08:47:19 +0200

    Work on Unit Tests

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -1,25 +1,25 @@
 package com.parkit.parkingsystem;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.FareCalculatorService;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.time.LocalDateTime;
 
 public class FareCalculatorServiceTest {
 
     private static FareCalculatorService fareCalculatorService;
     private Ticket ticket;
-
+    
     @BeforeAll
     private static void setUp() {
         fareCalculatorService = new FareCalculatorService();
@@ -29,7 +29,7 @@ public class FareCalculatorServiceTest {
     private void setUpPerTest() {
         ticket = new Ticket();
     }
-
+    
     @Test
     public void calculateFareCar(){
         LocalDateTime inTime = LocalDateTime.now().minusHours(1);
@@ -127,4 +127,33 @@ public class FareCalculatorServiceTest {
         assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
     }
 
+    @Test
+    public void calculateFareBikeWithMoreThanADayParkingTime(){
+    	LocalDateTime inTime = LocalDateTime.now().minusDays(1);// 1 day (= 24 hours) parking time should give 24 * parking fare per hour
+        //inTime.setTime( System.currentTimeMillis() - (  24 * 60 * 60 * 1000) );//24 hours parking time should give 24 * parking fare per hour
+        LocalDateTime outTime = LocalDateTime.now();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals( (24 * Fare.BIKE_RATE_PER_HOUR) , ticket.getPrice());
+    }
+    
+    
+    @Test
+    public void calculateFareCarWithNullOutTime(){
+    	LocalDateTime inTime = LocalDateTime.now().minusDays(1);// 1 day (= 24 hours) parking time should give 24 * parking fare per hour
+        //inTime.setTime( System.currentTimeMillis() - (  24 * 60 * 60 * 1000) );//24 hours parking time should give 24 * parking fare per hour
+        LocalDateTime outTime = null;
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        //fareCalculatorService.calculateFare(ticket);
+        //assertThrows(IllegalArgumentException.class, () -> fareCalculatorService.calculateFare(ticket));
+        assertThrows(NullPointerException.class, () -> fareCalculatorService.calculateFare(ticket));
+    }
 }
--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -8,14 +8,22 @@ import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,21 +38,26 @@ public class ParkingServiceTest {
     @Mock
     private static TicketDAO ticketDAO;
 
+    private LocalDateTime inTimeTest = LocalDateTime.now().minusHours(1);
+    
     @BeforeEach
     private void setUpPerTest() {
         try {
             when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
 
-            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+            
             Ticket ticket = new Ticket();
-            //ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));
-            ticket.setInTime(LocalDateTime.now().minusHours(1));
+            //ticket.setInTime(LocalDateTime.now().minusHours(1));
+            ticket.setInTime(inTimeTest);
             ticket.setParkingSpot(parkingSpot);
             ticket.setVehicleRegNumber("ABCDEF");
+            
             when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
-            when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+            
+            //when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
 
-            when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+            //when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
 
             parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         } catch (Exception e) {
@@ -54,9 +67,83 @@ public class ParkingServiceTest {
     }
 
     @Test
-    public void processExitingVehicleTest(){
-        parkingService.processExitingVehicle();
+    public void processExitingVehicleTestUpdateTicketTrue(){
+    	
+    	ArgumentCaptor<ParkingSpot> argumentCaptorParkingSpot = ArgumentCaptor.forClass(ParkingSpot.class);
+    	ArgumentCaptor<Ticket> argumentCaptorticket = ArgumentCaptor.forClass(Ticket.class);
+    	
+    	when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+    	
+    	when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+    	
+    	parkingService.processExitingVehicle();
+    	
         verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+        verify(ticketDAO, Mockito.times(1)).getTicket(anyString());
+        verify(ticketDAO, Mockito.times(1)).updateTicket(any(Ticket.class));
+                
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking(argumentCaptorParkingSpot.capture());
+       
+        ParkingSpot parkingSpotTest = argumentCaptorParkingSpot.getValue();
+        
+        assertEquals(1, parkingSpotTest.getId());
+        assertEquals(ParkingType.CAR, parkingSpotTest.getParkingType());
+        assertTrue(parkingSpotTest.isAvailable());
+   
+        verify(ticketDAO, Mockito.times(1)).updateTicket(argumentCaptorticket.capture());
+        
+        Ticket ticketTest = argumentCaptorticket.getValue();
+        
+        assertEquals(inTimeTest, ticketTest.getInTime());
+        assertEquals(new ParkingSpot(1, ParkingType.CAR, false), ticketTest.getParkingSpot());
+        assertEquals("ABCDEF", ticketTest.getVehicleRegNumber());
+            
     }
 
+    @Test
+    public void processExitingVehicleTestUpdateTicketFalse(){
+    	
+    	when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(false);
+    	
+    	parkingService.processExitingVehicle();
+    	
+        verify(parkingSpotDAO, Mockito.never()).updateParking(any(ParkingSpot.class));
+        verify(ticketDAO, Mockito.times(1)).getTicket(anyString());
+        verify(ticketDAO, Mockito.times(1)).updateTicket(any(Ticket.class));
+    }
+    
+    @Test
+    public void processExitingVehicleTestThrowException(){
+    	
+    	//when(ticketDAO.updateTicket(any(Ticket.class))).thenThrow(new Exception(""));  
+    	//assertThrows(Exception.class, () -> parkingService.processExitingVehicle());
+    	
+        Answer<Void> answerException = new Answer<Void>() 
+        {
+            @Override
+            public Void answer(InvocationOnMock invocationOnMock) 
+                                 throws Throwable {
+        
+            	throw new Exception("Test : exception getTicket()");
+            }			
+        };
+    	
+        when(ticketDAO.getTicket(anyString())).thenAnswer(answerException);
+    	        
+     	/*  	
+    	when(ticketDAO.getTicket(anyString())).thenAnswer(
+    		     new Answer<Void>() {
+    		      @Override
+    		      public Void answer(InvocationOnMock invocationOnMock) throws Exception {
+    		       
+    		    	  throw new Exception("Test : exception getTicket()");
+    		      }
+    		     });
+    	*/
+    	parkingService.processExitingVehicle();
+    	        
+    	verify(ticketDAO, Mockito.never()).updateTicket(any(Ticket.class));
+      	
+    }
+      
 }
